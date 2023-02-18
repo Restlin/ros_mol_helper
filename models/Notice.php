50f944e415c9b00f5714ee177409f8052a7f2839
restlin <restlin@dreamwalker>
2023-02-18 16:56:39 +0400

    [add] 1) fix message to telegram

--- models/Notice.php
+++ models/Notice.php
@@ -46,7 +46,7 @@ class Notice extends \yii\db\ActiveRecord
     }
 
     public function afterSave($insert, $changedAttributes) {
-        if($this->user->tg_id) {
+        if($insert && $this->user->tg_id) {
             $telegram = Yii::$container->get(\Longman\TelegramBot\Telegram::class);
             Request::sendMessage(['chat_id' => $this->user->tg_id, 'text' => $this->content]);
         }
